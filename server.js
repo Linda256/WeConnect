826fef72f3b77a7d71a0b0a8938c5a30dc378972
Linda Wang <lindawang256@gmail.com>
2018-10-08 17:12:11 -0700

    Add router

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
-const express=require('express');
+const express = require('express');
+const mongoose = require('mongoose');
+
 const app=express();
+const users=require('./routers/api/users');
+const profile=require('./routers/api/profile');
+const posts=require('./routers/api/posts');
+
+//DB config
+const db = require('./config/keys').mongoURI;
+
+//Connect db
+mongoose
+  .connect(db)
+  .then(()=>console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 app.get('/',(req,res) => res.send('Hello Linda'));
+
+//Use routes
+app.use('/api/users', users);
+app.use('/api/posts',posts);
+app.use('/api/profile',profile);
 
 const port = process.env.PORT || 5000;
 
