6d28ea9b4262fafd21a1682075cac85a0bf5ca57
Linda Wang <lindawang256@gmail.com>
2018-10-15 17:44:33 -0700

    Add delete user and profile

--- routers/api/profile.js
+++ routers/api/profile.js
@@ -262,4 +262,15 @@ router.delete('/education/:edu_id',passport.authenticate('jwt',{session:false}),
   }).catch(err=>res.status(404).json(err));
 })
 
+// @route DELETE api/profile
+// @desc Delete  profile and user
+// @access Private
+router.delete('/',passport.authenticate('jwt',{session:false}),(req,res)=>{
+  Profile.findOneAndRemove({user:req.user.id})
+  .then(()=>User.findOneAndRemove({_id:req.user.id}))
+  .then(()=>res.json("User is deleted"))
+})
+
+
+
 module.exports = router;
