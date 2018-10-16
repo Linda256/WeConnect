4e8b0df4565a3063cf4cf2e8b0d21bfe97d9ff51
Linda Wang <lindawang256@gmail.com>
2018-10-15 17:08:20 -0700

    Add education validation

---
+++ validation/education.js
@@ -0,0 +1,36 @@
+const validator = require('validator');
+const isEmpty =require('./isEmpty');
+
+function validateEducationInput(data){
+  let errors = {};
+
+  data.school = !isEmpty(data.school) ? data.school : '';
+  data.degree= !isEmpty(data.degree) ? data.degree : '';
+  data.fieldOfStudy= !isEmpty(data.fieldOfStudy) ? data.fieldOfStudy : '';
+  data.from= !isEmpty(data.from) ? data.from : ''
+  // data.to= !isEmpty(data.to) ? data.to : '';
+  // data.description= !isEmpty(data.description) ? data.description : ''
+
+  if(validator.isEmpty(data.school)){
+    errors.school = "School is required";
+  }
+
+  if(validator.isEmpty(data.degree)){
+    errors.degree= "Degree field is required";
+  }
+
+  if(validator.isEmpty(data.degree)){
+    errors.degree= "Degree field is required";
+  }
+
+  if(validator.isEmpty(data.from)){
+    errors.from= "From date field is required";
+  }
+
+  return {
+    errors:errors,
+    isValid: isEmpty(errors)
+  }
+}
+
+module.exports =  validateEducationInput;
